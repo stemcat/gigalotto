62feac7b043c770df5ca844a5481ae790e36c900
StemCat <82848892+stemcat@users.noreply.github.com>
2025-06-01 21:13:49 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { initWeb3, connectAndDeposit, withdrawWinnings, requestDraw } from './wa
 window.addEventListener('load', async () => {
   await initWeb3();
 
-  document.getElementById("depositBtn").addEventListener("click", connectAndDeposit);
-  document.getElementById("withdrawBtn").addEventListener("click", withdrawWinnings);
-  document.getElementById("requestDrawBtn").addEventListener("click", requestDraw);
+  document.getElementById("depositBtn")?.addEventListener("click", connectAndDeposit);
+  document.getElementById("withdrawBtn")?.addEventListener("click", withdrawWinnings);
+  document.getElementById("requestDrawBtn")?.addEventListener("click", requestDraw);
 });
